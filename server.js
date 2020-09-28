251d3ddf14a08dece6e86250c2aee79de961726c
chris grimley <chrismessage55@outlook.com>
2020-09-28 20:27:30 +0100

    first working version

--- server.js
+++ server.js
@@ -25,15 +25,12 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-  console.log(req.headers)
-  console.log(req.ip)
-});
-
+//whoami api endpoint
 app.get("/api/whoami", (req, res) => {
-  res.json({ipaddress: req.ip})
+  const ip = req.ip
+  const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
+  res.json({ipaddress: req.ip, language, software})
 })
 
 
